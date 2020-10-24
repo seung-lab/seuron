f87ea595802c562a34e42be4e73595e9e5d266ce
Ran Lu <ranl@princeton.edu>
2020-10-24 17:04:43 -0400

    No need to send success messages for every operator

--- dags/igneous_ops.py
+++ dags/igneous_ops.py
@@ -1,6 +1,6 @@
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils.weight_rule import WeightRule
-from slack_message import task_done_alert, task_retry_alert
+from slack_message import task_retry_alert
 from igneous_and_cloudvolume import downsample, downsample_for_meshing, mesh, mesh_manifest, create_skeleton_fragments, merge_skeleton_fragments
 from helper_ops import placeholder_op
 
@@ -16,7 +16,6 @@ def create_igneous_ops(param, dag):
                 task_id="downsample_with_mask",
                 python_callable=downsample_for_meshing,
                 op_args = [seg_cloudpath, param.get("SIZE_THRESHOLDED_MESH", False), ],
-                on_success_callback=task_done_alert,
                 on_retry_callback=task_retry_alert,
                 weight_rule=WeightRule.ABSOLUTE,
                 queue="manager",
@@ -30,7 +29,6 @@ def create_igneous_ops(param, dag):
             task_id="mesh",
             python_callable=mesh,
             op_args = [seg_cloudpath, param.get("MESH_QUALITY", "NORMAL"), ],
-            on_success_callback=task_done_alert,
             on_retry_callback=task_retry_alert,
             weight_rule=WeightRule.ABSOLUTE,
             queue="manager",
@@ -44,7 +42,6 @@ def create_igneous_ops(param, dag):
             task_id="mesh_manifest",
             python_callable=mesh_manifest,
             op_args = [seg_cloudpath, param["BBOX"], param["CHUNK_SIZE"], ],
-            on_success_callback=task_done_alert,
             on_retry_callback=task_retry_alert,
             weight_rule=WeightRule.ABSOLUTE,
             queue="manager",
@@ -60,7 +57,6 @@ def create_igneous_ops(param, dag):
                 task_id="downsample_ws",
                 python_callable=downsample,
                 op_args = [ws_cloudpath, ],
-                on_success_callback=task_done_alert,
                 on_retry_callback=task_retry_alert,
                 weight_rule=WeightRule.ABSOLUTE,
                 queue="manager",
@@ -74,7 +70,6 @@ def create_igneous_ops(param, dag):
             task_id="downsample_seg",
             python_callable=downsample,
             op_args = [seg_cloudpath, ],
-            on_success_callback=task_done_alert,
             on_retry_callback=task_retry_alert,
             weight_rule=WeightRule.ABSOLUTE,
             queue="manager",
@@ -88,7 +83,6 @@ def create_igneous_ops(param, dag):
             task_id="downsample_size_map",
             python_callable=downsample,
             op_args = [os.path.join(seg_cloudpath, "size_map"), ],
-            on_success_callback=task_done_alert,
             on_retry_callback=task_retry_alert,
             weight_rule=WeightRule.ABSOLUTE,
             queue="manager",
@@ -103,7 +97,6 @@ def create_igneous_ops(param, dag):
             task_id="skeleton_fragment",
             python_callable=create_skeleton_fragments,
             op_args = [seg_cloudpath, param.get("TEASAR_PARAMS", {'scale':10, 'const': 10}), ],
-            on_success_callback=task_done_alert,
             on_retry_callback=task_retry_alert,
             weight_rule=WeightRule.ABSOLUTE,
             queue="manager",
@@ -117,7 +110,6 @@ def create_igneous_ops(param, dag):
             task_id="merge_skeleton",
             python_callable=merge_skeleton_fragments,
             op_args = [seg_cloudpath, ],
-            on_success_callback=task_done_alert,
             on_retry_callback=task_retry_alert,
             weight_rule=WeightRule.ABSOLUTE,
             queue="manager",
