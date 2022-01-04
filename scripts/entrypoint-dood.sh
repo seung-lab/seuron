da0d1af60068a912477bc55cd70d00eafb458f14
Ran Lu <ranl@princeton.edu>
2022-01-04 18:05:06 -0500

    Fail other services before db is ready
    The approach is trying to set python_packages variable in the
    entrypoint, if that fails then fail the service

--- scripts/entrypoint-dood.sh
+++ scripts/entrypoint-dood.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+set -euo pipefail
+
 source scripts/add-user-docker.sh
 
 # this doesn't protect from docker but it's a little more secure
 sudo sed -i "/$AIRFLOW_USER/d" /etc/sudoers
 
+if [[ -n "${_AIRFLOW_DB_UPGRADE=}"  ]] ; then
+    if [ -z ${DOCKER_GROUP} ]; then
+        exec airflow db upgrade
+    else
+        exec sg ${DOCKER_GROUP} airflow db upgrade
+    fi
+fi
+
 echo "start script with group $DOCKER_GROUP"
+
+export PYTHONPATH=$AIRFLOW_HOME/common:$PYTHONPATH
 
 python scripts/install_packages.py
 
--- scripts/install_packages.py
+++ scripts/install_packages.py
@@ -5,11 +5,8 @@ import sys
 def install_package(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-packages = None
-try:
-    packages = Variable.get("python_packages")
-except:
-    pass
+Variable.setdefault("python_packages","")
+packages = Variable.get("python_packages")
 
 if packages:
     for l in packages.splitlines():
