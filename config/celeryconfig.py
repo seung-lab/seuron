17dfa0daa484ea7f5b0126f12cd6af2a223d3bf0
Ran Lu <ranl@princeton.edu>
2022-01-08 14:20:11 -0500

    Update CELERY_CONFIG

--- config/celeryconfig.py
+++ config/celeryconfig.py
@@ -2,19 +2,14 @@ from airflow import configuration
 
 # Broker settings.
 CELERY_CONFIG = {
-    'accept_content': ['json', 'pickle'],
-    'event_serializer': 'json',
-    'result_serializer': 'pickle',
-    'task_serializer': 'pickle',
-    #'task_time_limit': 7200,
     'worker_prefetch_multiplier': 1,
     'task_acks_late': True,
     'task_reject_on_worker_lost': True,
     'broker_url': configuration.get('celery', 'broker_url'),
-    'result_backend': configuration.get('celery', 'RESULT_BACKEND'),
+    'result_backend': configuration.get('celery', 'result_backend'),
     'worker_concurrency':
-        configuration.getint('celery', 'CELERYD_CONCURRENCY'),
-    'task_default_queue': configuration.get('operators', 'DEFAULT_QUEUE'),
-    'task_default_exchange': configuration.get('operators', 'DEFAULT_QUEUE'),
+        configuration.getint('celery', 'celeryd_concurrency'),
+    'task_default_queue': configuration.get('operators', 'default_queue'),
+    'task_default_exchange': configuration.get('operators', 'default_queue'),
     'worker_send_task_events': False
 }
