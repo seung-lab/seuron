c5419a2cef6025b97af59ef0522c85139812f3d5
Ran Lu <ranl@princeton.edu>
2022-04-26 20:06:53 -0400

    Obtain botUserID using slack api

--- slackbot/bot_info.py
+++ slackbot/bot_info.py
@@ -1,5 +1,13 @@
 from os import environ
+import slack_sdk as slack
+
+
+def get_botid():
+    client = slack.WebClient(token=slack_token)
+    auth_info = client.auth_test()
+    return f'<@{auth_info["user_id"]}>'
+
 slack_token = environ["SLACK_TOKEN"]
-botid = "<@{}>".format(environ["BOTUSERID"])
+botid = get_botid()
 workerid = "seuron-worker-"+environ["DEPLOYMENT"]
 broker_url = environ['AIRFLOW__CELERY__BROKER_URL']
