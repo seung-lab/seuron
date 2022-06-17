84356f2819186405a70deb767c8a1245ca35caf1
NT <nturner@zetta.ai>
2022-06-16 22:41:29 -0400

    feat: param file handles synaptor docker image
    Also adds synaptor default parameters to cluster start using
    init_pipeline.py

--- dags/synaptor_dags.py
+++ dags/synaptor_dags.py
@@ -5,7 +5,7 @@ from airflow import DAG
 from airflow.models import Variable
 
 from helper_ops import scale_up_cluster_op, scale_down_cluster_op, collect_metrics_op
-from param_default import default_synaptor_param
+from param_default import synaptor_param_default, default_synaptor_image
 from synaptor_ops import manager_op, drain_op
 from synaptor_ops import synaptor_op, wait_op, generate_op
 
@@ -14,9 +14,11 @@ from synaptor_ops import synaptor_op, wait_op, generate_op
 # We need to set a default so any airflow container can parse the dag before we
 # pass in a configuration file
 param = Variable.get(
-    "synaptor_param.json", default_synaptor_param, deserialize_json=True
+    "synaptor_param.json", synaptor_param_default, deserialize_json=True
 )
-MAX_CLUSTER_SIZE = int(param.get("Workflow", {}).get("maxclustersize", 1))
+WORKFLOW_PARAMS = param.get("Workflow", {})
+MAX_CLUSTER_SIZE = int(WORKFLOW_PARAMS.get("maxclustersize", 1))
+SYNAPTOR_IMAGE = WORKFLOW_PARAMS.get("synaptor_image", default_synaptor_image)
 
 default_args = {
     "owner": "seuronbot",
@@ -37,7 +39,7 @@ dag_sanity = DAG(
     tags=["synaptor"],
 )
 
-manager_op(dag_sanity, "sanity_check")
+manager_op(dag_sanity, "sanity_check", image=SYNAPTOR_IMAGE)
 
 
 # =========================================
@@ -55,8 +57,8 @@ fileseg_dag = DAG(
 # OP INSTANTIATION
 # Cluster management
 drain = drain_op(fileseg_dag)
-sanity_check = manager_op(fileseg_dag, "sanity_check")
-init_cloudvols = manager_op(fileseg_dag, "init_cloudvols")
+sanity_check = manager_op(fileseg_dag, "sanity_check", image=SYNAPTOR_IMAGE)
+init_cloudvols = manager_op(fileseg_dag, "init_cloudvols", image=SYNAPTOR_IMAGE)
 
 scale_up_cluster = scale_up_cluster_op(
     fileseg_dag, "synaptor", "synaptor-cpu", 1, MAX_CLUSTER_SIZE, "cluster"
@@ -65,20 +67,22 @@ scale_down_cluster = scale_down_cluster_op(
     fileseg_dag, "synaptor", "synaptor-cpu", 0, "cluster"
 )
 
-generate_self_destruct = generate_op(fileseg_dag, "self_destruct")
+generate_self_destruct = generate_op(fileseg_dag, "self_destruct", image=SYNAPTOR_IMAGE)
 wait_self_destruct = wait_op(fileseg_dag, "self_destruct")
 
 
 # Ops that do actual work
-workers = [synaptor_op(fileseg_dag, i) for i in range(MAX_CLUSTER_SIZE)]
+workers = [
+    synaptor_op(fileseg_dag, i, image=SYNAPTOR_IMAGE) for i in range(MAX_CLUSTER_SIZE)
+]
 
-generate_chunk_ccs = generate_op(fileseg_dag, "chunk_ccs")
+generate_chunk_ccs = generate_op(fileseg_dag, "chunk_ccs", image=SYNAPTOR_IMAGE)
 wait_chunk_ccs = wait_op(fileseg_dag, "chunk_ccs")
 
-generate_merge_ccs = generate_op(fileseg_dag, "merge_ccs")
+generate_merge_ccs = generate_op(fileseg_dag, "merge_ccs", image=SYNAPTOR_IMAGE)
 wait_merge_ccs = wait_op(fileseg_dag, "merge_ccs")
 
-generate_remap = generate_op(fileseg_dag, "remap")
+generate_remap = generate_op(fileseg_dag, "remap", image=SYNAPTOR_IMAGE)
 wait_remap = wait_op(fileseg_dag, "remap")
 
 
@@ -123,9 +127,9 @@ dbseg_dag = DAG(
 # OP INSTANTIATION
 # Cluster management
 drain = drain_op(dbseg_dag)
-sanity_check = manager_op(dbseg_dag, "sanity_check")
-init_cloudvols = manager_op(dbseg_dag, "init_cloudvols")
-init_db = manager_op(dbseg_dag, "init_db")
+sanity_check = manager_op(dbseg_dag, "sanity_check", image=SYNAPTOR_IMAGE)
+init_cloudvols = manager_op(dbseg_dag, "init_cloudvols", image=SYNAPTOR_IMAGE)
+init_db = manager_op(dbseg_dag, "init_db", image=SYNAPTOR_IMAGE)
 
 scale_up_cluster = scale_up_cluster_op(
     dbseg_dag, "synaptor", "synaptor-cpu", 1, MAX_CLUSTER_SIZE, "cluster"
@@ -134,29 +138,31 @@ scale_down_cluster = scale_down_cluster_op(
     dbseg_dag, "synaptor", "synaptor-cpu", 0, "cluster"
 )
 
-generate_self_destruct = generate_op(dbseg_dag, "self_destruct")
+generate_self_destruct = generate_op(dbseg_dag, "self_destruct", image=SYNAPTOR_IMAGE)
 wait_self_destruct = wait_op(dbseg_dag, "self_destruct")
 
 
 # Ops that do actual work
-workers = [synaptor_op(dbseg_dag, i) for i in range(MAX_CLUSTER_SIZE)]
+workers = [
+    synaptor_op(dbseg_dag, i, image=SYNAPTOR_IMAGE) for i in range(MAX_CLUSTER_SIZE)
+]
 
-generate_chunk_ccs = generate_op(dbseg_dag, "chunk_ccs")
+generate_chunk_ccs = generate_op(dbseg_dag, "chunk_ccs", image=SYNAPTOR_IMAGE)
 wait_chunk_ccs = wait_op(dbseg_dag, "chunk_ccs")
 
-generate_match_contins = generate_op(dbseg_dag, "match_contins")
+generate_match_contins = generate_op(dbseg_dag, "match_contins", image=SYNAPTOR_IMAGE)
 wait_match_contins = wait_op(dbseg_dag, "match_contins")
 
-generate_seg_graph_ccs = generate_op(dbseg_dag, "seg_graph_ccs")
+generate_seg_graph_ccs = generate_op(dbseg_dag, "seg_graph_ccs", image=SYNAPTOR_IMAGE)
 wait_seg_graph_ccs = wait_op(dbseg_dag, "seg_graph_ccs")
 
-generate_chunk_seg_map = generate_op(dbseg_dag, "chunk_seg_map")
+generate_chunk_seg_map = generate_op(dbseg_dag, "chunk_seg_map", image=SYNAPTOR_IMAGE)
 wait_chunk_seg_map = wait_op(dbseg_dag, "chunk_seg_map")
 
-generate_merge_seginfo = generate_op(dbseg_dag, "merge_seginfo")
+generate_merge_seginfo = generate_op(dbseg_dag, "merge_seginfo", image=SYNAPTOR_IMAGE)
 wait_merge_seginfo = wait_op(dbseg_dag, "merge_seginfo")
 
-generate_remap = generate_op(dbseg_dag, "remap")
+generate_remap = generate_op(dbseg_dag, "remap", image=SYNAPTOR_IMAGE)
 wait_remap = wait_op(dbseg_dag, "remap")
 
 
@@ -210,9 +216,9 @@ assign_dag = DAG(
 # OP INSTANTIATION
 # Cluster management
 drain = drain_op(assign_dag)
-sanity_check = manager_op(assign_dag, "sanity_check")
-init_cloudvols = manager_op(assign_dag, "init_cloudvols")
-init_db = manager_op(assign_dag, "init_db")
+sanity_check = manager_op(assign_dag, "sanity_check", image=SYNAPTOR_IMAGE)
+init_cloudvols = manager_op(assign_dag, "init_cloudvols", image=SYNAPTOR_IMAGE)
+init_db = manager_op(assign_dag, "init_db", image=SYNAPTOR_IMAGE)
 
 scale_up_cluster_cpu0 = scale_up_cluster_op(
     assign_dag, "synaptor-cpu0", "synaptor-cpu", 1, MAX_CLUSTER_SIZE, "cluster"
@@ -233,50 +239,66 @@ scale_down_cluster_cpu1 = scale_down_cluster_op(
     assign_dag, "synaptor-cpu1", "synaptor-cpu", 0, "cluster"
 )
 
-generate_self_destruct_cpu0 = generate_op(assign_dag, "self_destruct", tag="cpu0")
+generate_self_destruct_cpu0 = generate_op(
+    assign_dag, "self_destruct", tag="cpu0", image=SYNAPTOR_IMAGE
+)
 wait_self_destruct_cpu0 = wait_op(assign_dag, "self_destruct_cpu0")
-generate_self_destruct_gpu = generate_op(assign_dag, "self_destruct", tag="gpu")
+generate_self_destruct_gpu = generate_op(
+    assign_dag, "self_destruct", tag="gpu", image=SYNAPTOR_IMAGE
+)
 wait_self_destruct_gpu = wait_op(assign_dag, "self_destruct_gpu")
-generate_self_destruct_cpu1 = generate_op(assign_dag, "self_destruct", tag="cpu1")
+generate_self_destruct_cpu1 = generate_op(
+    assign_dag, "self_destruct", tag="cpu1", image=SYNAPTOR_IMAGE
+)
 wait_self_destruct_cpu1 = wait_op(assign_dag, "self_destruct_cpu1")
 
 
 # Ops that do actual work
-cpu0_workers = [synaptor_op(assign_dag, i, tag="cpu0") for i in range(MAX_CLUSTER_SIZE)]
-gpu_workers = [
-    synaptor_op(assign_dag, i, op_queue_name="synaptor-gpu", tag="gpu")
+cpu0_workers = [
+    synaptor_op(assign_dag, i, tag="cpu0", image=SYNAPTOR_IMAGE)
     for i in range(MAX_CLUSTER_SIZE)
 ]
-cpu1_workers = [synaptor_op(assign_dag, i, tag="cpu1") for i in range(MAX_CLUSTER_SIZE)]
+gpu_workers = [
+    synaptor_op(
+        assign_dag, i, op_queue_name="synaptor-gpu", tag="gpu", image=SYNAPTOR_IMAGE
+    )
+    for i in range(MAX_CLUSTER_SIZE)
+]
+cpu1_workers = [
+    synaptor_op(assign_dag, i, tag="cpu1", image=SYNAPTOR_IMAGE)
+    for i in range(MAX_CLUSTER_SIZE)
+]
 
-generate_chunk_ccs = generate_op(assign_dag, "chunk_ccs")
+generate_chunk_ccs = generate_op(assign_dag, "chunk_ccs", image=SYNAPTOR_IMAGE)
 wait_chunk_ccs = wait_op(assign_dag, "chunk_ccs")
 
-generate_match_contins = generate_op(assign_dag, "match_contins")
+generate_match_contins = generate_op(assign_dag, "match_contins", image=SYNAPTOR_IMAGE)
 wait_match_contins = wait_op(assign_dag, "match_contins")
 
-generate_seg_graph_ccs = generate_op(assign_dag, "seg_graph_ccs")
+generate_seg_graph_ccs = generate_op(assign_dag, "seg_graph_ccs", image=SYNAPTOR_IMAGE)
 wait_seg_graph_ccs = wait_op(assign_dag, "seg_graph_ccs")
 
-generate_chunk_seg_map = generate_op(assign_dag, "chunk_seg_map")
+generate_chunk_seg_map = generate_op(assign_dag, "chunk_seg_map", image=SYNAPTOR_IMAGE)
 wait_chunk_seg_map = wait_op(assign_dag, "chunk_seg_map")
 
-generate_merge_seginfo = generate_op(assign_dag, "merge_seginfo")
+generate_merge_seginfo = generate_op(assign_dag, "merge_seginfo", image=SYNAPTOR_IMAGE)
 wait_merge_seginfo = wait_op(assign_dag, "merge_seginfo")
 
-generate_chunk_edges = generate_op(assign_dag, "chunk_edges")
+generate_chunk_edges = generate_op(assign_dag, "chunk_edges", image=SYNAPTOR_IMAGE)
 wait_chunk_edges = wait_op(assign_dag, "chunk_edges")
 
-generate_pick_edge = generate_op(assign_dag, "pick_edge")
+generate_pick_edge = generate_op(assign_dag, "pick_edge", image=SYNAPTOR_IMAGE)
 wait_pick_edge = wait_op(assign_dag, "pick_edge")
 
-generate_merge_dups = generate_op(assign_dag, "merge_dups")
+generate_merge_dups = generate_op(assign_dag, "merge_dups", image=SYNAPTOR_IMAGE)
 wait_merge_dups = wait_op(assign_dag, "merge_dups")
 
-generate_merge_dup_maps = generate_op(assign_dag, "merge_dup_maps")
+generate_merge_dup_maps = generate_op(
+    assign_dag, "merge_dup_maps", image=SYNAPTOR_IMAGE
+)
 wait_merge_dup_maps = wait_op(assign_dag, "merge_dup_maps")
 
-generate_remap = generate_op(assign_dag, "remap")
+generate_remap = generate_op(assign_dag, "remap", image=SYNAPTOR_IMAGE)
 wait_remap = wait_op(assign_dag, "remap")
 
 
--- pipeline/init_pipeline.py
+++ pipeline/init_pipeline.py
@@ -2,7 +2,7 @@ from airflow.utils import db as db_utils
 from airflow.models import Variable
 from airflow import models
 from google_metadata import get_project_data, get_instance_data, get_instance_metadata, set_instance_metadata
-from param_default import param_default, inference_param_default
+from param_default import param_default, inference_param_default, synaptor_param_default
 import os
 import requests
 import json
@@ -46,6 +46,7 @@ Variable.setdefault("cluster_target_size", target_sizes, deserialize_json=True)
 Variable.setdefault("param", param_default, deserialize_json=True)
 Variable.setdefault("inference_param", inference_param_default, deserialize_json=True)
 Variable.setdefault("vendor", os.environ.get("VENDOR", "Google"))
+Variable.setdefault("synaptor_param.json", synaptor_param_default)
 
 db_utils.merge_conn(
         models.Connection(
