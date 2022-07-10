56e56e6bd9dbeaf746dcb8e65c018fac3178fbd8
Ran Lu <ranl@princeton.edu>
2022-07-10 14:32:40 -0400

    Assign names to redis databases

--- dags/param_default.py
+++ dags/param_default.py
@@ -67,6 +67,11 @@ default_args = {
     'max_retry_delay': timedelta(seconds=600)
     }
 
+redis_databases = {
+    'SEURON': 0,
+    'ABISS': 1,
+    'CHUNKFLOW': 2,
+}
 
 SLACK_CONN_ID = 'Slack'
 AWS_CONN_ID = 'AWS'
