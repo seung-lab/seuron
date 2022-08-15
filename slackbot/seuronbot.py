ae09f418cc558a45cf90d317c8c4dcd76531f806
Ran Lu <ranl@princeton.edu>
2022-08-15 16:43:22 -0400

    Split slack_bot.py
    Separate the part interact with slack into seuronbot.py, so we can
    replace the big function dispatching bot command with smaller function
    decorated by the decorator defined in seuronbot class.

---
+++ slackbot/seuronbot.py
@@ -0,0 +1,149 @@
+import re
+import functools
+import json
+from slack_sdk.rtm_v2 import RTMClient
+from bot_info import botid, workerid
+from bot_utils import replyto, extract_command
+from airflow_api import check_running
+
+class SeuronBot:
+    def __init__(self, slack_token=None):
+        self.help_listener = {
+            "triggers": ["help"],
+            "description": "Print help message",
+            "exclusive": False,
+            "extra_parameters": False,
+            "file_inputs": False,
+        }
+
+        trigger_cmds = ["".join(p.split()) for p in self.help_listener['triggers']]
+
+        def help_listener(context):
+            cmd = extract_command(context)
+            if cmd in trigger_cmds:
+                replyto(context, "Here is a list of the commands supported:\n"+self.generate_cmdlist())
+                return True
+            return False
+
+        self.help_listener['command'] = help_listener
+
+        self.message_listeners = [self.help_listener]
+
+        self.hello_listeners = []
+
+        self.rtmclient = RTMClient(token=slack_token)
+        self.rtmclient.on("message")(functools.partial(self.process_message.__func__, self))
+        self.rtmclient.on("reaction_added")(functools.partial(self.process_reaction.__func__, self))
+        self.rtmclient.on("hello")(functools.partial(self.process_hello.__func__, self))
+
+    def generate_command_description(self, cmd):
+        command = " or ".join(f"`{x}`" for x in cmd["triggers"])
+        if cmd['extra_parameters']:
+            command += " + _parameters_"
+        if cmd['file_inputs']:
+            command += " + _input file_"
+        if cmd['description']:
+            command += f"\n> {cmd['description']}"
+
+        return command
+
+    def generate_cmdlist(self):
+        return "\n".join(self.generate_command_description(listener) for listener in self.message_listeners)
+
+    def report(self, msg):
+        print("preparing report!")
+        if check_running():
+            replyto(msg, f"{workerid}: busy running segmentation for {task_owner}", username="seuronbot", broadcast=True)
+        else:
+            replyto(msg, f"{workerid}: idle", username="seuronbot", broadcast=True)
+
+    def filter_msg(self, msg):
+        if 'subtype' in msg and msg['subtype'] != "file_share":
+            return False
+        text = msg["text"].strip('''_*~"'`''')
+
+        if text.startswith(botid):
+            cmd = extract_command(msg)
+            if cmd == "report":
+                self.report(msg)
+            return False
+
+        if re.search(r"^{}[\s,:]".format(workerid), text, re.IGNORECASE):
+            return True
+
+    def process_message(self, client: RTMClient, event: dict):
+        print(json.dumps(event, indent=4))
+        if self.filter_msg(event):
+            handled = False
+            for listener in self.message_listeners:
+                handled |= listener["command"](event)
+
+            if not handled:
+                replyto(event, "Do not understand command")
+
+    def process_reaction(self, client: RTMClient, event: dict):
+        print("reaction added")
+        print(json.dumps(event, indent=4))
+
+
+    def process_hello(self, client: RTMClient, event: dict):
+        for listener in self.hello_listeners:
+            listener()
+
+    def run_cmd(self, func, context, exclusive=False):
+        if exclusive and check_running():
+            replyto(context, "Busy right now")
+            return
+        else:
+            func(context)
+
+    def on_hello(self):
+        def __call__(*args, **kwargs):
+            func = args[0]
+
+            def new_hello_listener():
+                func()
+
+            self.hello_listeners.append(new_hello_listener)
+
+            return func
+
+        return __call__
+
+    def on_message(self, trigger_phrases="", description="", exclusive=False, extra_parameters=False, file_inputs=False):
+        if isinstance(trigger_phrases, str):
+            trigger_phrases = [trigger_phrases]
+
+        def __call__(*args, **kwargs):
+            func = args[0]
+            trigger_cmds = ["".join(p.split()) for p in trigger_phrases]
+
+            def new_message_listener(context):
+                actual_cmd = extract_command(context)
+                if not extra_parameters:
+                    if actual_cmd in trigger_cmds:
+                        self.run_cmd(func, context, exclusive=exclusive)
+                        return True
+                else:
+                    for c in trigger_cmds:
+                        if actual_cmd.startswith(c):
+                            self.run_cmd(func, context, exclusive=exclusive)
+                            return True
+
+                return False
+
+            self.message_listeners.append({
+                "triggers": trigger_phrases,
+                "description": description,
+                "command": new_message_listener,
+                "exclusive": exclusive,
+                "extra_parameters": extra_parameters,
+                "file_inputs": file_inputs,
+            })
+
+            return func
+
+        return __call__
+
+    def start(self):
+        self.rtmclient.start()
