f5692399aa864df4acb3da1c55f88d7f328cb1d6
Ran Lu <ranl@princeton.edu>
2023-02-01 21:32:50 -0500

    Cleanup imports

--- slackbot/slack_bot.py
+++ slackbot/slack_bot.py
@@ -1,26 +1,14 @@
 import slack_sdk as slack
-import json
-import json5
-from collections import OrderedDict
-from airflow_api import get_variable, \
-    check_running, latest_dagrun_state, set_variable, \
-    mark_dags_success, run_dag
-from bot_info import slack_token, botid, workerid, broker_url, slack_notification_channel
-from kombu_helper import drain_messages, visible_messages, peek_message, get_message, put_message
-from bot_utils import fetch_slack_thread, replyto, extract_command, download_file, clear_queues
+from airflow_api import set_variable
+from bot_info import slack_token, workerid, broker_url, slack_notification_channel
+from kombu_helper import get_message
+from bot_utils import fetch_slack_thread
 from seuronbot import SeuronBot
-from google_metadata import get_project_data, get_instance_data, get_instance_metadata, set_instance_metadata, gce_external_ip
-from copy import deepcopy
+from google_metadata import gce_external_ip
 import time
 import logging
-from secrets import token_hex
-from datetime import datetime
-import threading
-import queue
 import sys
-import traceback
 import concurrent.futures
-import time
 
 import update_packages_commands
 import redeploy_commands
