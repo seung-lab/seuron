78df77633372ab248b0926cd8074ef1d3a84fcfb
Ran Lu <ranl@princeton.edu>
2023-04-11 14:32:14 -0400

    Rename POSTGRES_MEM to POSTGRES_MEM_MB

--- cloud/google/manager.py
+++ cloud/google/manager.py
@@ -18,7 +18,7 @@ def GenerateEnvironVar(context, hostname_manager):
         'POSTGRES_USER': context.properties['postgres']['user'],
         'POSTGRES_PASSWORD': context.properties['postgres']['password'],
         'POSTGRES_DB': context.properties['postgres']['database'],
-        'POSTGRES_MEM': """$(free -m|grep Mem|awk '{print int($2/4)}')""",
+        'POSTGRES_MEM_MB': """$(free -m|grep Mem|awk '{print int($2/4)}')""",
         'POSTGRES_MAX_CONN': """$(free -m|grep Mem|awk '{print int($2/32)}')""",
         'GRAFANA_USERNAME': context.properties['grafana']['user'],
         'GRAFANA_PASSWORD': context.properties['grafana']['password'],
