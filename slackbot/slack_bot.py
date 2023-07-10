075d10a9ad8f6573ad99df3906be1198e0c28cc3
Ran Lu <ranl@princeton.edu>
2023-07-10 14:17:49 -0400

    Send OOM to bot-message-queue as well

--- slackbot/slack_bot.py
+++ slackbot/slack_bot.py
@@ -1,15 +1,11 @@
 import slack_sdk as slack
 from airflow_api import set_variable
-from bot_info import slack_token, workerid, broker_url, slack_notification_channel
-from kombu_helper import get_message
-from bot_utils import fetch_slack_thread
+from bot_info import slack_token, workerid, slack_notification_channel
 from seuronbot import SeuronBot
 
 import os
-import time
 import logging
 import sys
-import concurrent.futures
 
 if os.environ.get("VENDOR", None) == "Google":
     from google_metadata import gce_external_ip
@@ -58,26 +54,8 @@ def hello_world(client=None):
         text="Hello from <https://{}/airflow/home|{}>".format(host_ip, host_ip))
 
 
-def fetch_oom_messages(queue="worker-message-queue"):
-    client = slack.WebClient(token=slack_token)
-    while True:
-        msg = get_message(broker_url, queue, timeout=30)
-        if msg:
-            slack_workername, slack_info = fetch_slack_thread()
-            slack_username = slack_info["user"]
-            client.chat_postMessage(
-                username=slack_workername,
-                channel=slack_info["channel"],
-                thread_ts=slack_info["thread_ts"],
-                text=f"<@{slack_username}>, {msg}"
-            )
-            time.sleep(1)
-
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
     seuronbot = SeuronBot(slack_token=slack_token)
-    executor = concurrent.futures.ThreadPoolExecutor(max_workers=1)
-    f = executor.submit(fetch_oom_messages)
     seuronbot.start()
