f01f458d1bbf78d795f1e1e18283885cd78246ac
Ran Lu <ranl@princeton.edu>
2023-10-02 18:11:07 -0400

    Add an easyseg_dag
    For the moment it just starts the easyseg worker

---
+++ dags/easyseg_dag.py
@@ -0,0 +1,32 @@
+import pendulum
+from airflow.decorators import dag, task
+
+@dag(
+    schedule=None,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["maintenance, segmentation, inference"],
+)
+def easyseg_dag():
+    @task(queue="cluster", priority_weight=1000)
+    def start_worker():
+        from slack_message import slack_message
+
+        if Variable.get("vendor") == "Google":
+            import google_api_helper as cluster_api
+        else:
+            cluster_api = None
+
+        if cluster_api is None:
+            return
+
+        slack_message(":exclamation:*Turn on easyseg worker*")
+        try:
+            cluster_api.toggle_easyseg_worker(on=True)
+        except Exception:
+            pass
+
+    start_worker()
+
+
+easyseg_dag()
