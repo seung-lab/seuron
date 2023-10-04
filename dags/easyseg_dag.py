3f8008429cf73d1f379c4f8b0ca222e1bc80296c
Ran Lu <ranl@princeton.edu>
2023-10-04 18:14:19 -0400

    Update heartbeat for easyseg

--- dags/easyseg_dag.py
+++ dags/easyseg_dag.py
@@ -10,7 +10,12 @@ from airflow.decorators import dag, task
 def easyseg_dag():
     @task(queue="cluster", priority_weight=1000)
     def start_worker():
+        import os
+        import redis
+        from datetime import datetime
         from slack_message import slack_message
+        from airflow.models import Variable
+        from airflow.hooks.base_hook import BaseHook
 
         if Variable.get("vendor") == "Google":
             import google_api_helper as cluster_api
@@ -25,6 +30,15 @@ def easyseg_dag():
             cluster_api.toggle_easyseg_worker(on=True)
         except Exception:
             pass
+
+        ig_conn = BaseHook.get_connection("EasysegWorker")
+        deployment = ig_conn.host
+        instance = f"{deployment}-easyseg-worker"
+
+        redis_host = os.environ['REDIS_SERVER']
+        timestamp = datetime.now().timestamp()
+        r = redis.Redis(redis_host)
+        r.set(instance, timestamp)
 
     start_worker()
 
