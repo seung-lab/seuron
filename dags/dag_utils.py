3b2dd5c26de7456c4c063e03783b65dd07452f46
Ran Lu <ranl@princeton.edu>
2024-03-21 16:49:46 -0400

    Check available_mem instead of total_mem for memory constraint
    For small instances the available_mem can be significantly less than total_mem.
    
    Also make the error message more visible

--- dags/dag_utils.py
+++ dags/dag_utils.py
@@ -2,10 +2,10 @@ def check_manager_node(ntasks):
     import psutil
     import humanize
     from slack_message import slack_message
-    total_mem = psutil.virtual_memory().total
+    available_mem = psutil.virtual_memory().available
     expected_mem = 2*ntasks*1024*1024*10 # 10MB for each postgresql connection
-    if total_mem < expected_mem:
-        slack_message(f"You need {humanize.naturalsize(expected_mem)} RAM to handle {ntasks} tasks, the manager node only have {humanize.naturalsize(total_mem)} RAM")
+    if available_mem < expected_mem:
+        slack_message(f":u7981:*ERROR: You need {humanize.naturalsize(expected_mem)} RAM to handle {ntasks} tasks, the manager node only have {humanize.naturalsize(total_mem)} RAM*")
         return False
 
     return True
