4fc4b7268aa9e349683ceec15ad60eca9c0770bd
Ran Lu <ranl@princeton.edu>
2024-11-28 22:36:55 -0500

    Control normalization in the default shader for affinity

--- dags/chunkflow_dag.py
+++ dags/chunkflow_dag.py
@@ -53,7 +53,7 @@ def generate_ng_link():
 
     layers["out"] = {
         "source": "precomputed://"+param["OUTPUT_PATH"],
-        "shader": param.get("OUTPUT_SHADER", "void main() {\n  float r = toNormalized(getDataValue(0));\n  float g = toNormalized(getDataValue(1));\n  float b = toNormalized(getDataValue(2)); \n  emitRGB(vec3(r,g,b));\n}"),
+        "shader": param.get("OUTPUT_SHADER", "#uicontrol invlerp normalized\nvoid main() {\n  float r = toNormalized(getDataValue(0));\n  float g = toNormalized(getDataValue(1));\n  float b = toNormalized(getDataValue(2)); \n  emitRGB(vec3(r,g,b));\n}"),
         "type": param.get("OUTPUT_LAYER_TYPE", "image")
     }
 
